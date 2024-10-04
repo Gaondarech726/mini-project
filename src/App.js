81f219adae47617d8b6b7d316dd0d290400e7d96
K. L <121451594+madparrot69@users.noreply.github.com>
2024-10-04 20:58:30 +0300

    hero
    hero jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/hero/hero";
 import WishesForm from "./components/wishesForm/wishesForm";
 
 function App() {
   return (
     <>
       <WishesForm></WishesForm>
+      <Hero></Hero>
     </>
   );
 }
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,1 +1,46 @@
-console.log("hi");
+import "./hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="heroHeader">
+      <div className="heroAuthorsDeserts">
+        <h1 className="heroName">Авторські десерти </h1>
+
+        <div className="heroPiesContainer">
+          <div className="heroWhiskDiv">
+            <img src="./img/venchik.png" alt="" />
+          </div>
+
+          <div className="heroCakeDiv">
+            <img src="./img/cake.png" alt="" />
+            <h4>мусові торти</h4>
+          </div>
+
+          <div className="heroStrangePieDiv">
+            <img src="./img/strange_pie.png" alt="" />
+            <h4>заварні шу</h4>
+          </div>
+
+          <div className="heroPieDiv">
+            <img src="./img/pie.png" alt="" />
+            <h4>апетитні тарти</h4>
+          </div>
+
+          <div className="heroPDiv">
+            <p className="heroP">
+              Виготовляємо десерти під замовлення з любов’ю та користю
+            </p>
+          </div>
+        </div>
+
+        <div className="heroSocialMedia">
+          <button className="heroTelegramBtn">T</button>
+          <button className="heroFacebookBtn">F</button>
+          <button className="heroInstagramBtn">I</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
