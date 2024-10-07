2ecc3fccf0c4813fa8c5917ffaa29e99fea8d9c8
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-10-07 20:07:14 +0300

    style for wishesForm__textContent

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import About from "./components/about/about";
+import Hero from "./components/hero/hero";
 import WishesForm from "./components/wishesForm/wishesForm";
 
 function App() {
--- src/components/wishesForm/wishesForm.jsx
+++ src/components/wishesForm/wishesForm.jsx
@@ -1,35 +1,13 @@
-import "./wishesForm.scss";
+import Form from "./form/form";
+import TextContent from "./textContent/textContent";
+import "./wishesFormStyle.scss";
 
 const WishesForm = () => {
   return (
-    <>
-      <div className="textContent__wishesForm">
-        <h1 className="title__wishesForm">
-          Давайте зробимо щось унікальне разом!
-        </h1>
-        <p className="text__wishesForm">
-          Ви можете замовити свій власний рецепт десерту, який вам буде до
-          вподоби. Заповніть заявку та я зв’яжусь з вами, аби обговорити деталі
-          замовлення.
-        </p>
-      </div>
-      <div className="formContent__wishesForm">
-        <form>
-          <input name="name" placeholder="Ім'я" type="text"></input>
-          <input
-            name="numberPhone"
-            placeholder="Номер телефону"
-            type="number"
-          ></input>
-          <input
-            name="basicText"
-            placeholder="Опишіть ваші побажання:"
-            type="text"
-          ></input>
-          <button type="sumbit">Надіслати</button>
-        </form>
-      </div>
-    </>
+    <section className="wishesForm">
+      <TextContent></TextContent>
+      <Form></Form>
+    </section>
   );
 };
 
