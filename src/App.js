0f795fe7bc74554b7303b19179d4d5ff93bba72d
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2024-10-07 15:10:25 +0300

    save gallery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import WishesForm from "./components/wishesForm/wishesForm";
 import Footer from "./components/footer/Footer";
+import Gallery from "./components/gallery/gallery"
 
 function App() {
   return (
     <>
+      <Gallery></Gallery>
       <WishesForm></WishesForm>
       <Footer></Footer>
     </>
---
+++ src/components/gallery/gallery.jsx
@@ -0,0 +1,33 @@
+import "./gallery.scss";
+import cakeImg from "./images/Rectangle 7.png";
+import ShuImg from "./images/Rectangle 14.png";
+import miniCakeImg from "./images/Rectangle 15.png";
+import pizzaImg from "./images/Rectangle 16.png";
+
+const Gallery = () => {
+  return (
+    <div>
+    <div className="Gallery">
+      <div className="choose">
+        <img src={cakeImg} alt="" />
+            <p>ТОРТИ</p>
+            <button className="moreCakes">→</button>
+      </div>
+      <div className="choose">
+        <img src={ShuImg} alt="" />
+        <p>ШУ</p>
+      </div>
+      <div className="choose">
+        <img src={miniCakeImg} alt="" />
+        <p>ТІСТЕЧКА</p>
+      </div>
+      <div className="choose">
+        <img src={pizzaImg} alt="" />
+        <p>ПІЦА</p>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Gallery
