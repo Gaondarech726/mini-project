e3858f37106e1201d40e98412558634b3864e055
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-10-08 21:05:00 +0300

    footerProblem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import About from "./components/about/about";
+import Footer from "./components/footer/footer";
+import Gallery from "./components/gallery/gallery";
 import Hero from "./components/hero/hero";
 import WishesForm from "./components/wishesForm/wishesForm";
 
 function App() {
   return (
     <>
+      <About></About>
+      <Hero></Hero>
+      <Gallery></Gallery>
       <WishesForm></WishesForm>
+      <Footer></Footer>
     </>
   );
 }
-
 export default App;
