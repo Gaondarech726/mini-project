3e2b62258b552f612b6a9db1d61ab6800789cfa1
Dremeret <terletskyyd2010@gmail.com>
2024-10-12 18:06:09 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-<<<<<<< HEAD
+
 import Body from "./components/body";
 import Footer from "./components/footer/footer.jsx";
 import Header from "./components/header/header.jsx";
-=======
 import "../src/index.css";
 import About from "./components/about/about";
 import Footer from "./components/footer/footer";
 import Gallery from "./components/gallery/gallery";
 import Hero from "./components/hero/hero";
->>>>>>> 6e343ef440b72e05556c0d925b22182e25aa11ef
+
 import WishesForm from "./components/wishesForm/wishesForm";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
+
       <WishesForm></WishesForm>
       <Header></Header>
       <Body></Body>
-=======
+
       <Hero></Hero>
       <About></About>
       <Gallery></Gallery>
       <WishesForm></WishesForm>
->>>>>>> 6e343ef440b72e05556c0d925b22182e25aa11ef
+
       <Footer></Footer>
     </>
   );
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 6e343ef440b72e05556c0d925b22182e25aa11ef
+
+
 export default App;
