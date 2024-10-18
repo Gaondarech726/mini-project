d0849454563097b923d77445ae7012f7939af161
Svyatoslav <best.goodx@gmail.com>
2024-10-18 18:23:58 +0300

    Revert "Merge branch 'main' of https://github.com/Gaondarech726/mini-project"
    This reverts commit 9b9c60ab3359a5d3dc6644f42da3c3a46aab5dde, reversing
    changes made to 0f3184fe79c3be4b0f51868c9384fdcd85eef91e.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-
-import Body from "./components/body";
-import Footer from "./components/footer/footer.jsx";
-import Header from "./components/header/header.jsx";
 import "../src/index.css";
 import About from "./components/about/about";
 import Footer from "./components/footer/footer";
 import Gallery from "./components/gallery/gallery";
 import Hero from "./components/hero/hero";
-
 import WishesForm from "./components/wishesForm/wishesForm";
 
 function App() {
   return (
     <>
-
-      <WishesForm></WishesForm>
-      <Header></Header>
-      <Body></Body>
-
       <Hero></Hero>
       <About></About>
       <Gallery></Gallery>
       <WishesForm></WishesForm>
-
       <Footer></Footer>
     </>
   );
 }
-
-
-
 export default App;
